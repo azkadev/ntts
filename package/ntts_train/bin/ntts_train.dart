548c0bb5c2dfaa693ac38f9ad551031d32f4c6e8
Your Name <you@example.com>
2023-04-17 19:36:07 +0700

    update

---
+++ package/ntts_train/bin/ntts_train.dart
@@ -0,0 +1,5 @@
+import 'package:ntts_train/ntts_train.dart' as ntts_train;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ntts_train.calculate()}!');
+}
---
+++ package/ntts_train/test/ntts_train_test.dart
@@ -0,0 +1,8 @@
+import 'package:ntts_train/ntts_train.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

d4eadaaf18a624b14834e36011051ddba50557d4
Your Name <you@example.com>
2023-11-21 00:26:11 +0700

    add interactive cli

--- package/ntts_dart/bin/ntts_dart.dart
+++ package/ntts_dart/bin/ntts_dart.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
+import 'dart:isolate';
+
 import 'package:general_lib/general_lib.dart';
+import 'package:mason_logger/mason_logger.dart';
 import 'package:ntts_dart/api/create_voice.dart';
 import 'package:ntts_dart/ntts_dart.dart';
 import 'package:ntts_dart/scheme/scheme.dart';
@@ -7,23 +10,14 @@ import 'package:play/play_dart.dart';
 import "package:path/path.dart" as path;
 
 void main(List<String> arguments) async {
+  Logger logger = Logger();
+  logger.info("Started NTTS DART - AZKADEV");
   Args args = Args(arguments);
-  Ntts ntts = Ntts(
-    pathLib: "libntts.so",
-  );
-  File file = File(args.after("-m") ?? path.join(Directory.current.path, "en_US-libritts-high.onnx"));
-  if (!file.existsSync()) {
-    print("Please add model");
-    exit(0);
-  }
-
-  File file_ouput = File(path.join(Directory.current.path, "data.wav"));
-  if (file_ouput.existsSync()) {
-    await file_ouput.delete();
-  }
-  var res = ntts.invokeRaw(
-    data: CreateVoice.create(
-      text: args.after("-t") ??  """
+  String file_model = args.after("-m") ?? path.join(Directory.current.path, "en_US-libritts-high.onnx");
+  Progress progress_load_model = logger.progress("Load Model");
+  int speaker_id = int.tryParse(args.after("-s") ?? "10") ?? 10;
+  String text = args.after("-t") ??
+      """
 This sound is created using dart language by azkadev without using any internet
 
 Human: Good afternoon, robot. How are you today?
@@ -65,30 +59,73 @@ Human: No, that's all for now. Thank you, robot.
 Robot: You're welcome. Don't hesitate to contact me if you need further assistance.
 
 """
-          .trim(),
-      model_path: file.path,
-      output_file: file_ouput.path,
-      speaker_id: int.tryParse(args.after("-s") ?? "10") ?? 10,
-    ).toJson(),
+          .trim();
+  await Future.delayed(Duration(milliseconds: 500));
+  File file = File(file_model);
+  String file_model_path = file.path;
+  progress_load_model.update("Check Model File: ${file.path}");
+  await Future.delayed(Duration(milliseconds: 500));
+  if (!file.existsSync()) {
+    progress_load_model.fail("Not Found Model File: ${file.path}");
+    await Future.delayed(Duration(milliseconds: 500));
+    exit(0);
+  } else {
+    progress_load_model.complete("Found Model File: ${file.path}");
+    await Future.delayed(Duration(milliseconds: 500));
+  }
+
+  File file_ouput = File(path.join(Directory.current.path, "data.wav"));
+  String file_output_sound = file_ouput.path;
+  if (file_ouput.existsSync()) {
+    await file_ouput.delete();
+  }
+
+  Progress progress_create_sound = logger.progress("Create Sound");
+  await Future.delayed(Duration(milliseconds: 500));
+  var res = await Isolate.run<Map>(() {
+    Ntts ntts = Ntts(
+      pathLib: "libntts.so",
+    );
+
+    var res = ntts.invokeRaw(
+      data: CreateVoice.create(
+        text: text,
+        model_path: file_model_path,
+        output_file: file_output_sound,
+        speaker_id: speaker_id,
+      ).toJson(),
+    );
+
+    return res;
+  });
+  await Future.delayed(Duration(milliseconds: 500));
+  progress_create_sound.complete("Succes Create Sound");
+  await Future.delayed(Duration(milliseconds: 500));
+  Voice voice = Voice(res);
+  Progress progress_play_sound = logger.progress("Started Play Sound");
+  Play play = Play();
+
+  await Future.delayed(Duration(milliseconds: 500));
+  await play.open(
+    medias: [
+      voice.output_file ?? "",
+    ],
+    play: false,
   );
 
-  Voice voice = Voice(res);
-
-  Play play = Play();
-  await play.open(medias: [
-    voice.output_file ?? "",
-  ]);
-  play.player.stream.error.listen((event) {
-    print(event);
-  });
+  await play.play();
   // play.player.stream;
   play.player.stream.completed.listen(
-    (event) {
+    (event) async {
       if (event) {
+        progress_play_sound.complete("Finished");
+        await Future.delayed(Duration(seconds: 2));
         exit(0);
       }
     },
-    onDone: () {
+    onDone: () async {
+      progress_play_sound.complete("Finished");
+      await Future.delayed(Duration(seconds: 2));
       exit(0);
     },
   );
